3d533b2ce7169e4e82102d6f45d04e9faf2ce5a6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-05 11:14:35 +0000

    Seção 3-Fundamentos - Aula - 16 - Comentários de Código

--- Fundamentos/Comentarios.cs
+++ Fundamentos/Comentarios.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.Fundamentos
-{
+{   
+    /// <summary>
+    /// XML coments
+    /// </summary>
     class Comentarios
     {
         public static void Executar()
         {
             //Cuidado dom os comentários desnecessários
+            /*
+             Este é um comentário
+             de multiplas linhas 
+             */
             Console.WriteLine("Código claro é sempre melhor!");
             Console.WriteLine("O C# tem o XML Comments");
         }
