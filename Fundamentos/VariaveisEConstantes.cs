585fb4a8d2ea66e696d2bfaf4989eb4f21c35b69
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-05 11:30:11 +0000

    Seção 3-Fundamentos - Aula - 17 - Variáveis e Constantes

---
+++ Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCsharp.Fundamentos
+{
+    class VariaveisEConstantes
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace CursoCsharp
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
+                {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
             });
 
             central.SelecionarEExecutar();
