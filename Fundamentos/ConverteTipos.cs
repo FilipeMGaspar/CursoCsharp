f49e428f097527c3d8751d2322669d0e550b986b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-10 10:28:31 +0000

    Seção 3-Fundamentos - Aula 23 - Conversões de Tipos de Dados

--- Fundamentos/ConverteTipos.cs
+++ Fundamentos/ConverteTipos.cs
@@ -10,6 +10,10 @@ namespace CursoCsharp.Fundamentos
         {
             int inteiro = 10;
             double quebrado = inteiro;
+            Console.WriteLine(quebrado);
+
+            double nota = 9.7;
+            int notaTruncada = nota;
         }
     }
 }
