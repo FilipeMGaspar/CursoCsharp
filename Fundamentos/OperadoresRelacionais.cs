7bea730ceb0166412cc869ad9aa33c5200e89b88
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-10 14:54:31 +0000

    Seção 3-Fundamentos - Aula 25 - Operadores Relacionais

--- Fundamentos/OperadoresRelacionais.cs
+++ Fundamentos/OperadoresRelacionais.cs
@@ -8,6 +8,16 @@ namespace CursoCsharp.Fundamentos
     {
         public static void Executar()
         {
+            double nota = 6.0;
+            double notaDeCorte = 7.0;
+
+            Console.WriteLine("Nota Invélida: {0} ", nota > 10.0);
+            Console.WriteLine("Nota inválida: {0} ", nota < 0.0);
+            Console.WriteLine("Perfeito: {0}", nota == 10);
+            Console.WriteLine("Tem como melhorar: {0}", nota != 10);
+            Console.WriteLine("Passou por média: {0}", nota >= notaDeCorte);
+            Console.WriteLine("Recuperação: {0}", nota < notaDeCorte);
+            Console.WriteLine("Reprovado: {0}", nota <= 3.0);
 
         }
     }
