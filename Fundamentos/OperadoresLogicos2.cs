d3ba7f3ed70ea34ccec3800c2301a19f1f44f008
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-11 15:15:24 +0000

    Seção 3 - Fundamentos - Aula 27 - Operadores Lógicos #02

--- Fundamentos/OperadoresLogicos2.cs
+++ Fundamentos/OperadoresLogicos2.cs
@@ -12,10 +12,10 @@ namespace CursoCsharp.Fundamentos
             var executouTrabalho2 = false;
 
             var comprouTv50 = executouTrabalho1 && executouTrabalho2;
-            Console.WriteLine("Comprou Tv 50 {0}", comprouTv50);
+            Console.WriteLine("Comprou Tv 50: {0}", comprouTv50);
 
             var comprouSorvete = executouTrabalho1 || executouTrabalho2;
-            Console.WriteLine("Comprou sorbete {0}", comprouSorvete);
+            Console.WriteLine("Comprou sorbete: {0}", comprouSorvete);
 
             var comprouTv32 = executouTrabalho1 ^ executouTrabalho2;
             Console.WriteLine("Comprou a Tv 32: {0}", comprouTv32);
