1d1e5f292168022baa514e7a5ba8f0c9b4a66f44
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-11 14:49:25 +0000

    Seção 3 - Fundamentos - Aula 26 - Operadores Lógicos #01

---
+++ Fundamentos/OperadoresLogicos1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCsharp.Fundamentos
+{
+    class OperadoresLogicos1
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace CursoCsharp
                 {"Conversões de Tipos de Dados - Fundamentos", ConverteTipos.Executar},
                 {"Operadores Aritméticos - Fundamentos", OperadoresAritmeticos.Executar},
                 {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
+                {"Operadores Lógicos #01 - Fundamentos", OperadoresLogicos1.Executar},
             });
 
             central.SelecionarEExecutar();
