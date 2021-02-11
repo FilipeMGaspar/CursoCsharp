48246bf4dc6738b5242a91a7f8137dc4fd93413d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-11 17:40:11 +0000

    Seção 3 - Fundamentos - Aula 29 - Operadores Unários

--- Fundamentos/OperadoresUnarios.cs
+++ Fundamentos/OperadoresUnarios.cs
@@ -8,6 +8,13 @@ namespace CursoCsharp.Fundamentos
     {
         public static void Executar()
         {
+            var valorNegativo = -5;
+            var numero1 = 2;
+            var numero2 = 3;
+            var booleano = true;
+
+            Console.WriteLine(-valorNegativo);
+            Console.WriteLine(!booleano);
 
         }
     }
