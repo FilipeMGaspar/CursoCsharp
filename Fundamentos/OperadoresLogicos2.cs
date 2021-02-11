975ab372198abef3a9e03e53e6dc3c8ba00f1406
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-11 15:16:43 +0000

    Seção 3 - Fundamentos - Aula 27 - Operadores Lógicos #02

--- Fundamentos/OperadoresLogicos2.cs
+++ Fundamentos/OperadoresLogicos2.cs
@@ -8,8 +8,8 @@ namespace CursoCsharp.Fundamentos
     {
         public static void Executar()
         {
-            var executouTrabalho1 = true;
-            var executouTrabalho2 = true;
+            var executouTrabalho1 = false;
+            var executouTrabalho2 = false;
 
             var comprouTv50 = executouTrabalho1 && executouTrabalho2;
             Console.WriteLine("Comprou Tv 50: {0}", comprouTv50);
