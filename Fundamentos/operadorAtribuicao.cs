45d034446e2b244d35efc478227cc74363d89ca8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-11 17:26:49 +0000

    Seção 3 - Fundamentos - Aula 28 - Operadores de Atribuição

--- Fundamentos/operadorAtribuicao.cs
+++ Fundamentos/operadorAtribuicao.cs
@@ -10,8 +10,12 @@ namespace CursoCsharp.Fundamentos
         {
             var num1 = 3;
             num1 = 7;
-            num1 += 10; // num1 = num1 + 10
+            num1 += 10; // num1 = num1 + 10;
+            num1 -= 3; // num1 = num1 -3;
+            num1 *= 5; // num1 = num1 * 5;
+            num1 /= 2; //num1 = num1 /2;
 
+            Console.WriteLine(num1);
         }
     }
 }
