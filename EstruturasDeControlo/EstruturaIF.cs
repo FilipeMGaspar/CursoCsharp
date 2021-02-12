2f6449a6421892fc9001fed3f4a998dc643bfcf8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-12 13:59:02 +0000

    Seção 4 - Estruturas de Controle - Aula 31 - Estrutura IF

---
+++ EstruturasDeControlo/EstruturaIF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCsharp.EstruturasDeControlo
+{
+    class EstruturaIF
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
