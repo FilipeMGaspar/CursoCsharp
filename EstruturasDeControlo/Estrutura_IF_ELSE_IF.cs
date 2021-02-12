569c6b8cf24bcf2c77ee500cf76526a89ae6aef8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-12 14:40:23 +0000

    Seção 4 - Estruturas de Controle - Aula 33 - Estrutura IF/ELSE IF

--- EstruturasDeControlo/Estrutura_IF_ELSE_IF.cs
+++ EstruturasDeControlo/Estrutura_IF_ELSE_IF.cs
@@ -8,7 +8,19 @@ namespace CursoCsharp.EstruturasDeControlo
     {
         public static void Executar()
         {
+            Console.Write("Digite a nota do aluno: ");
+            string entrada = Console.ReadLine();
+            Double.TryParse(entrada, out double nota);
 
+            if(nota >= 9.0)
+            {
+                Console.WriteLine("Quadro de Honra!");
+            }
+            else {
+                if (nota >= 7.0 && nota < 9.0){
+                    Console.WriteLine("Aprovado!");
+                }
+            }
         }
     }
 }
