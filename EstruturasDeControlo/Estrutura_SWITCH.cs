87c94c5c74129aa58c9cdd0a859e7e9b2a9fea85
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-12 15:08:11 +0000

    Seção 4 - Estruturas de Controle - Aula 34 - Estrutura SWITCH

--- EstruturasDeControlo/Estrutura_SWITCH.cs
+++ EstruturasDeControlo/Estrutura_SWITCH.cs
@@ -8,7 +8,32 @@ namespace CursoCsharp.EstruturasDeControlo
     {
         public static void Executar()
         {
+            Console.Write("Avalie o meu Atendimento: [de 0 a 5]");
+            int.TryParse(Console.ReadLine(), out int nota);
 
+            switch (nota) {
+                case 0:
+                    Console.WriteLine("Pésimo");
+                    break;
+                case 1:
+                case 2:
+                    Console.WriteLine("Mau");
+                    break;
+                case 3:
+                    Console.WriteLine("Suficiente");
+                    break;
+                case 4:
+                    Console.WriteLine("Bom");
+                    break;
+                case 5:
+                    Console.WriteLine("Excelente");
+                    break;
+                default:
+                    Console.WriteLine("Avaliação inválida!");
+                    break;
+            }
+            
+            Console.WriteLine("Fim da avaliação");
         }
     }
 }
