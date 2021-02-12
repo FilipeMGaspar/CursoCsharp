12b079b470b9d988f978c0dae0f7bfe1528ace84
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-12 14:19:00 +0000

    Seção 4 - Estruturas de Controle - Aula 31 - Estrutura IF

--- EstruturasDeControlo/EstruturaIF.cs
+++ EstruturasDeControlo/EstruturaIF.cs
@@ -21,7 +21,8 @@ namespace CursoCsharp.EstruturasDeControlo
             //if (entrada == "S" || entrada == "s")
                 //bomComportamento = true;
             // outra forma  
-            bomComportamento = entrada == "S" || entrada == "s";
+            //bomComportamento = entrada == "S" || entrada == "s";
+            bomComportamento = entrada.ToLower() == "s";
 
             if (nota >= 9.0 && bomComportamento)
                 Console.WriteLine("Quadro de Honra");
