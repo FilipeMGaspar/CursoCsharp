a8020f476da06bffef620341c22e042fb37f0550
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-13 17:01:08 +0000

    Seção 5 - Classes & Métodos - Aula 43 - Membros, Atributos e Métodos

--- ClassesEMetodos/Membros.cs
+++ ClassesEMetodos/Membros.cs
@@ -4,6 +4,13 @@ using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
     class Membros {
+        public static void Executar() {
+            Pessoa sicrano = new Pessoa();
 
+            sicrano.Nome = "Renato";
+            sicrano.Idade = 35;
+
+            Console.WriteLine($"O {sicrano.Nome} tem {sicrano.Idade} anos.");
+        }
     }
 }
