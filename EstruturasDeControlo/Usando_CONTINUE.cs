5be1877521ecc83103884d4db29375f84e92c7bf
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-13 15:40:08 +0000

    Seção 4 - Estruturas de Controle - Aula 40 - Usando CONTINUE

--- EstruturasDeControlo/Usando_CONTINUE.cs
+++ EstruturasDeControlo/Usando_CONTINUE.cs
@@ -7,7 +7,16 @@ namespace CursoCsharp.EstruturasDeControlo
     class Usando_CONTINUE
     {
         public static void Executar() {
+            int intervalo = 50;
 
+            Console.WriteLine("Números pares entre 1 e {0}", intervalo);
+
+            for (int i = 1; i<=intervalo; i++) {
+                if (i % 2 == 1){
+                    continue;
+                }
+                Console.Write(i + " ");
+            }
         }
     }
 }
