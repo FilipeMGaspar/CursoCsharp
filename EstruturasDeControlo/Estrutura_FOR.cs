22ffaaf7da210361d3f8f4baea3d6b6245622602
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-13 14:52:13 +0000

    Seção 4 - Estruturas de Controle - Aula 37 - Estrutura FOR

--- EstruturasDeControlo/Estrutura_FOR.cs
+++ EstruturasDeControlo/Estrutura_FOR.cs
@@ -8,7 +8,19 @@ namespace CursoCsharp.EstruturasDeControlo
     {
         public static void Executar()
         {
-            
+            double somatorio = 0;
+            string entrada;
+
+            Console.Write("Informe o tamanho da turma: ");
+            entrada = Console.ReadLine();
+            int.TryParse(entrada, out int tamanhoTurma);
+
+            for (int i = 1; i<= tamanhoTurma; i++) {
+                Console.WriteLine($"Nota do {i}º aluno: ");
+                entrada = Console.ReadLine();
+
+            }
+
             //Estrutora de repetição for
             //for (int i = 0; i<=10; i++) {
             //    Console.WriteLine($"O valor de i é : {i}");
