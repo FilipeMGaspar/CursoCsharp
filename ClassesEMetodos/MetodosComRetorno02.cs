281534aa894c0830abc865fa80f4cb1158609b0d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 11:09:08 +0000

    Seção 5 - Classes & Métodos - Aula 46 - Métodos Com Retorno #02

--- ClassesEMetodos/MetodosComRetorno02.cs
+++ ClassesEMetodos/MetodosComRetorno02.cs
@@ -20,6 +20,35 @@ namespace CursoCsharp.ClassesEMetodos {
             return a / b;
         }
     }
+
+    class CalculadoraCadeia {
+        int memoria;
+
+        public CalculadoraCadeia Somar(int a) {
+            memoria += a;
+            return this;
+        }
+
+        public CalculadoraCadeia Multiplicar(int a) {
+            memoria *= a;
+            return this;
+        }
+
+        public CalculadoraCadeia Limpar() {
+            memoria = 0;
+            return this;
+        }
+
+        public CalculadoraCadeia Imprimir() {
+            Console.WriteLine(memoria);
+            return this;
+        }
+
+        public int Resultado() {
+            return memoria;
+        }
+    }
+
     class MetodosComRetorno02 {
         public static void Executar() {
             var calculadoraComum = new CalculadoraComum();
@@ -29,6 +58,10 @@ namespace CursoCsharp.ClassesEMetodos {
             Console.WriteLine(calculadoraComum.subtrair(2, 7));
             Console.WriteLine(calculadoraComum.Multiplicar(4, 4));
             Console.WriteLine(calculadoraComum.Dividir(24, 3));
+
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+            calculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
         }
     }
 }
