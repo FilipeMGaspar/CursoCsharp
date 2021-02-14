058375caedd76c82a749c5d5cb398be1b51f75a8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 11:57:33 +0000

    Aula 39 - Usando BREAK -> Seção 4: Estruturas de Controle

--- EstruturasDeControlo/Usando_BREAK.cs
+++ EstruturasDeControlo/Usando_BREAK.cs
@@ -12,7 +12,7 @@ namespace CursoCsharp.EstruturasDeControlo
             int numero = random.Next(1, 51); // número aleatóreo entre 1 e 50
 
             Console.WriteLine("O número a encontrar: {0}", numero);
-
+            //utlilização do break; não deve ser utilizado
             for (int i = 1; i<=50; i++) {
                 Console.Write("{0} é o numero que qeremos? ", i);
                 if (i == numero) {
