7b83c45cb8d4145ef9597cc083797845031bf412
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 11:48:14 +0000

    Aula 30 - Operador Ternário -> Seção 3: Fundamentos

--- Fundamentos/OperadoreTernario.cs
+++ Fundamentos/OperadoreTernario.cs
@@ -8,6 +8,7 @@ namespace CursoCsharp.Fundamentos
     {
         public static void Executar()
         {
+           //operador Ternário
             var nota = 5.0;
             bool bomComportamento = true;
             string resultado = nota >= 7.0 && bomComportamento ? "Aprovado" : "Reprovado"; //Operador ternário
