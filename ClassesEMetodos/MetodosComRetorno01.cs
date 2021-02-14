84206388cbde252440c8d982f11d7225d144d8c1
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 10:53:25 +0000

    Seção 5 - Classes & Métodos - Aula 45 -Métodos Com Retorno #01

--- ClassesEMetodos/MetodosComRetorno01.cs
+++ ClassesEMetodos/MetodosComRetorno01.cs
@@ -8,6 +8,14 @@ namespace CursoCsharp.ClassesEMetodos {
         public int Somar(int a, int b) {
             return a + b;
         }
+
+        public int subtrair(int a, int b) {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b) {
+            return a * b;
+        }
     }
 
     class MetodosComRetorno01 {
