4f4fe861c4e8f13f1ee826167830cf502cc3d01f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 11:01:52 +0000

    Seção 5 - Classes & Métodos - Aula 46 - Métodos Com Retorno #02

--- ClassesEMetodos/MetodosComRetorno02.cs
+++ ClassesEMetodos/MetodosComRetorno02.cs
@@ -3,9 +3,32 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
+    class CalculadoraComum {
+        public int Somar(int a, int b) {
+            return a + b;
+        }
+
+        public int subtrair(int a, int b) {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b) {
+            return a * b;
+        }
+
+        public int Dividir(int a, int b) {
+            return a / b;
+        }
+    }
     class MetodosComRetorno02 {
         public static void Executar() {
+            var calculadoraComum = new CalculadoraComum();
+            var resultado = calculadoraComum.Somar(8, 3);
 
+            Console.WriteLine(resultado);
+            Console.WriteLine(calculadoraComum.subtrair(2, 7));
+            Console.WriteLine(calculadoraComum.Multiplicar(4, 4));
+            Console.WriteLine(calculadoraComum.Dividir(24, 3));
         }
     }
 }
