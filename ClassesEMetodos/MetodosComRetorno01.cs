2845eca4cd809345fc62b5fa02894e0e9f2e25b3
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 10:58:15 +0000

    Seção 5 - Classes & Métodos - Aula 45 -Métodos Com Retorno #01

--- ClassesEMetodos/MetodosComRetorno01.cs
+++ ClassesEMetodos/MetodosComRetorno01.cs
@@ -16,16 +16,21 @@ namespace CursoCsharp.ClassesEMetodos {
         public int Multiplicar(int a, int b) {
             return a * b;
         }
+
+        public int Dividir(int a, int b) {
+            return a / b;
+        }
     }
 
     class MetodosComRetorno01 {
         public static void Executar() {
             var calculadoraComum = new CalculadoraComum();
-            var resultado = calculadoraComum.Somar(2, 3);
+            var resultado = calculadoraComum.Somar(8, 3);
 
             Console.WriteLine(resultado);
             Console.WriteLine(calculadoraComum.subtrair(2, 7));
             Console.WriteLine(calculadoraComum.Multiplicar(4, 4));
+            Console.WriteLine(calculadoraComum.Dividir(24, 3));
         }
     }
 }
