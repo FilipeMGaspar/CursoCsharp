2ece7808d20a459a3106e8f9e9415b8c35d9da8f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 10:46:41 +0000

    Seção 5 - Classes & Métodos - Aula 45 -Métodos Com Retorno #01

--- ClassesEMetodos/MetodosComRetorno01.cs
+++ ClassesEMetodos/MetodosComRetorno01.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
+
+    class CalculadoraComum {
+        public int Somar(int a, int b) {
+            return a + b;
+        }
+    }
+
     class MetodosComRetorno01 {
+        public static void Executar() {
+
+        }
     }
 }
