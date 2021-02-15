3fe80677130b5f239a5f729a8b6b82252333570a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-15 10:22:46 +0000

    Aula 47 - Métodos Estáticos -> Seção 5: Classes & Métodos

--- ClassesEMetodos/MetodosEstaticos.cs
+++ ClassesEMetodos/MetodosEstaticos.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace CursoCsharp.ClassesEMetodos {
     class MetodosEstaticos {
         public static void Executar() {
-            //
+            //Métodos estáticos
+
         }
     }
 }
