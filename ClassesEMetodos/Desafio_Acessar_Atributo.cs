a51ffa3214191771bc7d1f4feabb064a116c3635
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-15 11:13:16 +0000

    Aula 50 - Desafio Acessar Atributo (Resposta) -> Seção 5: Classes & Métodos

--- ClassesEMetodos/Desafio_Acessar_Atributo.cs
+++ ClassesEMetodos/Desafio_Acessar_Atributo.cs
@@ -5,10 +5,12 @@ using System.Text;
 namespace CursoCsharp.ClassesEMetodos {
     class Desafio_Acessar_Atributo {
 
-        int a = 10;
+        int a = 10;// Variavel de instancia só possivel acessar através de construtor
         public static void Executar() {
             // Acessar a variavel a dentro método executar
-            Desafio_Acessar_Atributo atributo = new Desafio_Acessar_Atributo();
+            //Console.WriteLine(a); // Resposta errada
+            
+            Desafio_Acessar_Atributo atributo = new Desafio_Acessar_Atributo();//Construtor para a instancia
             Console.WriteLine(atributo.a);
         }
 
