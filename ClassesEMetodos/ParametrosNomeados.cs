a4f181823649cd6d9cc71fe8a9baea62d68fc13a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-15 11:32:46 +0000

    Aula 52 - Parâmetros Nomeados -> Seção 5: Classes & Métodos

--- ClassesEMetodos/ParametrosNomeados.cs
+++ ClassesEMetodos/ParametrosNomeados.cs
@@ -6,7 +6,7 @@ namespace CursoCsharp.ClassesEMetodos {
     class ParametrosNomeados {
 
         public static void Formatar(int dia, int mes, int ano) {
-            Console.WriteLine("{0:D2}/{}", dia);
+            Console.WriteLine("{0:D2}/{1:d2}/{2}", dia, mes, ano);
         }
 
         public static void Executar() {
