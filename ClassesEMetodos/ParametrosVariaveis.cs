9bdc6397bf8abe9132db04c4787e9c41c6662854
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-15 11:20:40 +0000

    Aula 51 - Parâmetros Variáveis -> Seção 5: Classes & Métodos

--- ClassesEMetodos/ParametrosVariaveis.cs
+++ ClassesEMetodos/ParametrosVariaveis.cs
@@ -4,6 +4,11 @@ using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
     class ParametrosVariaveis {
+
+        public static void recepcionar(params string[] pessoas) {
+
+        }
+
         public static void Executar() {
 
         }
