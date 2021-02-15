2ee209a3a9ff43f52b953b92c3ac5516eaa601ff
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-15 10:39:46 +0000

    Aula 48 - Atributos Estáticos -> Seção 5: Classes & Métodos

--- ClassesEMetodos/AtributosEstaticos.cs
+++ ClassesEMetodos/AtributosEstaticos.cs
@@ -4,6 +4,24 @@ using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
     class AtributosEstaticos {
+
+        public string Nome;
+        public double Preco;
+        public double Desconto = 0.1;
+
+        public Produto(string nome, double preco, double desconto) {
+            Nome = nome;
+            Preco = preco;
+            Desconto = desconto;
+        }
+
+        public Produto() {
+
+        }
+
+        public double CalculadoraDesconto() {
+            return Preco - Preco * Desconto;
+        }
         public static void Executar() {
 
         }
