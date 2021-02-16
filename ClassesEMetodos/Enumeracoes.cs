b7e9331ddcdb05e15e368cfcce6492ee0842195e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 12:08:51 +0000

    Aula 56 - Enumerações (Enum) -> Seção 5: Classes & Métodos

--- ClassesEMetodos/Enumeracoes.cs
+++ ClassesEMetodos/Enumeracoes.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
+
+    public enum Genero { Acao, Aventura, Terror, Animacao, Comedia }
     class Enumeracoes {
         public static void Executar() {
 
