8b721f32d4ea82cb40c8669050966dcfd641c977
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 12:29:06 +0000

    Aula 57 - Struct -> Seção 5: Classes & Métodos

--- ClassesEMetodos/Struct.cs
+++ ClassesEMetodos/Struct.cs
@@ -8,6 +8,15 @@ namespace CursoCsharp.ClassesEMetodos {
         void MoverNaDiagonal(int delta);// método de interface tem de ser público, definição por defeito
     }
 
+    struct Coordenada: Ponto {
+        public int X;
+        public int Y;
+
+        public Coordenada(int x, int y) {
+            X = x;
+            Y = y;
+        }
+    } 
 
     class Struct {
         public static void Executar() {
