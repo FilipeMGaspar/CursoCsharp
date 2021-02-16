cc5f8acc1df5540da9745ee29eb372785dd78976
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 11:47:06 +0000

    Aula 55 - Atributos Readonly -> Seção 5: Classes & Métodos

--- ClassesEMetodos/AtributosReadonly.cs
+++ ClassesEMetodos/AtributosReadonly.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
+
+    public class Cliente {
+        public string Nome;
+        DateTime Nascimente; //Private DateTime Nascimente;
+
+        public Cliente(string nome, DateTime nascimento) {
+            Nome = nome;
+
+        }
+    }
+
     class AtributosReadonly {
         public static void Executar() {
 
