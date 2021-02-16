14ee6a14be4a6d9245923821dc6c2fc0e78ab4c5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 12:25:38 +0000

    Aula 57 - Struct -> Seção 5: Classes & Métodos

--- ClassesEMetodos/Struct.cs
+++ ClassesEMetodos/Struct.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
+
+    interface Ponto {
+        void MoverNaDiagonal(int delta);
+    }
+
     class Struct {
         public static void Executar() {
 
