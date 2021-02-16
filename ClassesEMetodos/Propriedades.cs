a17a79bcc66f3690f8a784d3a77bf6aec4a779a8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 11:16:38 +0000

    Aula 54 - Propriedades -> Seção 5: Classes & Métodos

--- ClassesEMetodos/Propriedades.cs
+++ ClassesEMetodos/Propriedades.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
+
+    public class CarroOpcional {
+        double desconto = 0.1; //variavél privada -> private double desconto = 0.1;
+        string nome;//variavél privada -> private string nome;
+
+        public string Nome;
+    }
+
     class Propriedades {
         public static void Executar() {
 
