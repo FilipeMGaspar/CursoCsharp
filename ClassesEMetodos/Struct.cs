ef9533f55c977d9c9e761bfeac044de0b9cd3cb0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 12:37:53 +0000

    Aula 57 - Struct -> Seção 5: Classes & Métodos

--- ClassesEMetodos/Struct.cs
+++ ClassesEMetodos/Struct.cs
@@ -29,8 +29,15 @@ namespace CursoCsharp.ClassesEMetodos {
             coordenadaInicial.X = 2;
             coordenadaInicial.Y = 2;
 
+            Console.WriteLine("Coordenada Inicial: ");
             Console.WriteLine("X = {0}", coordenadaInicial.X);
             Console.WriteLine("Y = {0}", coordenadaInicial.Y);
+
+            var coordenadaFinal = new Coordenada(x: 9, y: 1);
+            coordenadaFinal.MoverNaDiagonal(10);
+
+            Console.WriteLine("Coordenada Final:");
+
         }
     }
 }
