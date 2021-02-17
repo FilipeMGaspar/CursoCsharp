5bab6c58fd000c764821362a6d3372f3a43f00d7
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-17 13:31:36 +0000

    Aula 58 - Class vs Struct -> Seção 5: Classes & Métodos

--- ClassesEMetodos/Class_Vs_Struct.cs
+++ ClassesEMetodos/Class_Vs_Struct.cs
@@ -4,6 +4,14 @@ using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
     class Class_Vs_Struct {
+
+        public struct SPonto {
+            public int X;
+            public int Y;
+        }
+
+        
+
         public static void Executar() {
 
         }
