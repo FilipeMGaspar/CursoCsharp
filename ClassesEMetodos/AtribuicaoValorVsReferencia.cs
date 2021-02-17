859f8fd1226a0a0251725e3ac5ce8c00eb79e508
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-17 13:59:29 +0000

    Aula 59 - Atribuição por Valor vs Referência -> Seção 5: Classes & Métodos

--- ClassesEMetodos/AtribuicaoValorVsReferencia.cs
+++ ClassesEMetodos/AtribuicaoValorVsReferencia.cs
@@ -4,8 +4,19 @@ using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
     class AtribuicaoValorVsReferencia {
-        public static void Executar() {
 
+        public class Dependente {
+            public string Nome;
+            public int idade;
+        }
+
+        public static void Executar() {
+            int numero = 3;
+            int copiaNumero = numero;
+            Console.WriteLine($"{numero} {copiaNumero}");
+
+            numero++;
+            Console.WriteLine($"{numero} {copiaNumero}");
         }
     }
 }
