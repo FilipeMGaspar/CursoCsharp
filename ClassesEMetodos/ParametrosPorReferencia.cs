705f9da51401e82d79a109d851311498c192ba80
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-17 14:25:42 +0000

    Aula 60 - Parâmetros por Referência (Ref/Out) -> Seção 5: Classes & Métodos

--- ClassesEMetodos/ParametrosPorReferencia.cs
+++ ClassesEMetodos/ParametrosPorReferencia.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos {
+
     class ParametrosPorReferencia {
+        public static void AlterarRef(int numero) {
+            numero = numero + 1000; 
+        }
+
+        public static void AlterarOut(int numero) {
+            numero = numero + 15;
+        }
+
         public static void Executar() {
 
         }
