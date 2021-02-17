b3dbf208c6015635120e6f0439db8b35f393a944
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-17 13:36:03 +0000

    Aula 58 - Class vs Struct -> Seção 5: Classes & Métodos

--- ClassesEMetodos/Class_Vs_Struct.cs
+++ ClassesEMetodos/Class_Vs_Struct.cs
@@ -17,7 +17,11 @@ namespace CursoCsharp.ClassesEMetodos {
 
         public static void Executar() {
             SPonto ponto1 = new SPonto { X = 1, Y = 3 };
+            SPonto copiaPonto1 = ponto1;
+            ponto1.X = 3;
 
+            Console.WriteLine("Ponto 1 X:{0}", ponto1.X);
+            Console.WriteLine("Copia Ponto 1 X:{0}",);
         }
     }
 }
