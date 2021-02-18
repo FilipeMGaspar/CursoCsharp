5362cd784621d3bbc664d625b6d62a9f65a339f5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-18 12:37:43 +0000

    Aula 63 - List -> Seção 6: Coleções

--- Colecoes/List.cs
+++ Colecoes/List.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.Colecoes {
+
+    public class Produto {
+        public string Nome;
+        public double Preco;
+    }
+
+    public Produto(string nome, double preco) {
+        Nome = nome;
+        preco = preco;
+    }
+
     class List {
         public static void Executar() {
 
