1cd7fb02f2ad72bb308ba89d0114de517e2b7403
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-18 13:10:53 +0000

    Aula 64 - ArrayList -> Seção 6: Coleçõe

--- Colecoes/ArrayList.cs
+++ Colecoes/ArrayList.cs
@@ -6,8 +6,14 @@ namespace CursoCsharp.Colecoes {
     class ArrayList {
         public static void Executar() {
             var arraylist = new ArrayList {
-
-            }
+                "Palavra",
+                3,
+                true
+            };
         }
+
+        arraylist.Add(3.14);
+
+
     }
 }
