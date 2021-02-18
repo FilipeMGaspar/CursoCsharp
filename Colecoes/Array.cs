1ea822faea28430fc95ab5e9a5eb15fc1c50f823
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-18 11:20:54 +0000

    Aula 62 - Array -> Seção 6: Coleções

--- Colecoes/Array.cs
+++ Colecoes/Array.cs
@@ -5,7 +5,13 @@ using System.Text;
 namespace CursoCsharp.Colecoes {
     class Array {
         public static void Executar() {
-
+            string[] alunos = new string[5];//todos os dados são do mesmo tipo e um tamanho constante
+            alunos[0] = "Anderson";
+            alunos[1] = "Bia";
+            alunos[2] = "Carlos";
+            alunos[3] = "Daniela";
+            alunos[4] = "Emanuel";
+            alunos[5] = "Emanuel";
         }
     }
 }
