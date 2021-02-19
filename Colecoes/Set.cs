565e5ab58dd122bc67715fb9d11382eeaced6c7f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-19 09:45:10 +0000

    Aula 65 - Set -> Seção 6: Coleções

--- Colecoes/Set.cs
+++ Colecoes/Set.cs
@@ -5,7 +5,31 @@ using System.Text;
 namespace CursoCsharp.Colecoes {
     class Set {
         public static void Executar() {
+            var livro = new Produto("Game of Throne", 22.9);
 
+            var carrinho = new List<Produto>();
+            carrinho.Add(livro);
+
+            var combo = new List<Produto> {
+                new Produto("Camisa", 29.9),
+                new Produto("Jogo MotoGp", 35.99),
+                new Produto("Poster", 10)
+            };
+            carrinho.AddRange(combo);
+            Console.WriteLine(carrinho.Count);
+
+            Console.WriteLine();
+            carrinho.RemoveAt(3);
+            foreach (var item in carrinho) {
+                Console.WriteLine(carrinho.IndexOf(item));
+                Console.WriteLine($"{item.Nome} {item.Preco}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(carrinho.Count);
+            carrinho.Add(livro);
+            Console.WriteLine();
+            Console.WriteLine(carrinho.LastIndexOf(livro));
         }
     }
 }
