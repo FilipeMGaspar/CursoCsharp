4a901e12d2e590cfde0762dbb761de67175511c5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-19 10:19:44 +0000

    Aula 67 - Igualdade (Equals e GetHashCode) -> Seção 6: Coleções

--- Colecoes/Igualdade.cs
+++ Colecoes/Igualdade.cs
@@ -5,10 +5,17 @@ using System.Text;
 namespace CursoCsharp.Colecoes {
     class Igualdade {
         public static void Executar() {
+            // Produto contem string nome e double preco
+
             var p1 = new Produto("Caneta", 1.89);
             var p2 = new Produto("Caneta", 1.89);
+            var p3 = p2;
 
             Console.WriteLine(p1 == p2);
+            Console.WriteLine(p3 == p2);
+            
+            
+            Console.WriteLine(p1.Equals(p2));
         }
     }
 }
