1c2faf8c53ce83682c2748c1f152c1b2d4f4dd5e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-19 09:57:44 +0000

    Aula 66 - Queue -> Seção 6: Coleções

--- Colecoes/Queue.cs
+++ Colecoes/Queue.cs
@@ -5,6 +5,12 @@ using System.Text;
 namespace CursoCsharp.Colecoes {
     class Queue {
         public static void Executar() {
+            var fila = new Queue<string>();
+
+            fila.Enqueue("Fulano");
+            fila.Enqueue("Sicrano");
+            fila.Enqueue("Beltrano");
+
 
         }
     }
