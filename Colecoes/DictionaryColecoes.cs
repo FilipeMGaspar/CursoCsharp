8fa144f9ca15e2e2d7eefe5712aeec9c0cfa3d4f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-19 10:55:45 +0000

    Aula 69 - Dictionary -> Seção 6: Coleções

--- Colecoes/DictionaryColecoes.cs
+++ Colecoes/DictionaryColecoes.cs
@@ -5,7 +5,12 @@ using System.Text;
 namespace CursoCsharp.Colecoes {
     class DictionaryColecoes {
         public static void Executar() {
+            var filmes = new Dictionary<int, string>();//a chave int não aceita repetição
 
+            filmes.Add(2000, "Gladiador");
+            filmes.Add(2002, "Homem-Aranha");
+            filmes.Add(2004, "Os Incríveis");
+            filmes.Add(2006, "O Grande Truque");
         }
     }
 }
