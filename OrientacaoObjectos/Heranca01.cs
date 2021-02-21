c78d2e2a9c1b11dcd75fa0a6d5f7cd55baab6f4b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-21 12:17:01 +0000

    Aula 74 - Herança #01 -> Seção 7 - Orientação a Objetos

--- OrientacaoObjectos/Heranca01.cs
+++ OrientacaoObjectos/Heranca01.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.OrientacaoObjectos {
+
+    public class Carro {
+        protected readonly int VelocidadeMaxima;
+        private int VelocidadeAtual;
+
+        public Carro(int velocidadeMaxima) {
+            VelocidadeMaxima = velocidadeMaxima;
+        }
+    }
+
+
     class Heranca01 {
         public static void Executar() {
 
