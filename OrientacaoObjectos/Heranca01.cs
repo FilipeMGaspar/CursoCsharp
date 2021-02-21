a281d6f0b070f58f74df30526407774bf7da9a81
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-21 12:19:05 +0000

    Aula 74 - Herança #01 -> Seção 7 - Orientação a Objetos

--- OrientacaoObjectos/Heranca01.cs
+++ OrientacaoObjectos/Heranca01.cs
@@ -5,11 +5,15 @@ using System.Text;
 namespace CursoCsharp.OrientacaoObjectos {
 
     public class Carro {
-        protected readonly int VelocidadeMaxima;
+        protected readonly int VelocidadeMaxima;//Vai ser passado por herança
         private int VelocidadeAtual;
 
         public Carro(int velocidadeMaxima) {
             VelocidadeMaxima = velocidadeMaxima;
+        }
+
+        protected int AlterarVelocidade(int delta) {//transmitido por herança
+            int novaVelocidade;//variavel interna do método
         }
     }
 
