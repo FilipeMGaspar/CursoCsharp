943758feb75e1439d8b7caee9de3f4af3565a58f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-21 16:53:39 +0000

    Aula 76 - Construtor Usando o this -> Seção 7 - Orientação a Objetos

--- OrientacaoObjectos/UsandoThis.cs
+++ OrientacaoObjectos/UsandoThis.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.OrientacaoObjectos {
+
+    public class Animal {
+        public string Nome { get; set; }
+    }
+
     class UsandoThis {
         public static void Executar() {
 
