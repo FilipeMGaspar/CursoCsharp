159b53d09a7da88fe8eb3a165f2ed63389c826f2
CursoC# <fipewmc2@gmail.com>
2021-02-23 15:57:49 +0000

    Aula 77 - Encapsulamento -> Seção 7 - Orientação a Objetos

---
+++ OrientacaoObjectos/Encapsulamento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCsharp.OrientacaoObjectos {
+    class Encapsulamento {
+        public static void Executar() {
+
+        }
+    }
+}
