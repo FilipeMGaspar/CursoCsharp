e0961a24213054a700cede7f945e5c427741a214
CursoC# <fipewmc2@gmail.com>
2021-02-23 17:51:52 +0000

    Aula 77 - Encapsulamento -> Seção 7 - Orientação a Objetos

--- OrientacaoObjectos/Encapsulamento.cs
+++ OrientacaoObjectos/Encapsulamento.cs
@@ -5,7 +5,15 @@ using Encapsulamento;
 namespace CursoCsharp.OrientacaoObjectos {
 
     public class FilhoNaoReconhecido : SubCelebridade {
-        public new void 
+        public new void MeusAcessos() {
+            Console.WriteLine("## FilhoNãoReconhecido ##");
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoDeFamilia);
+            //Console.WriteLine(UsaMuitoPhotoshop);
+        }
     }
 
     class Encapsulamento {
