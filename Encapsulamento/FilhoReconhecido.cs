60f5974d3b2fd8fb1496b1acfca13209d60ec0ed
CursoC# <fipewmc2@gmail.com>
2021-02-23 17:31:00 +0000

    Aula 77 - Encapsulamento -> Seção 7 - Orientação a Objetos

--- Encapsulamento/FilhoReconhecido.cs
+++ Encapsulamento/FilhoReconhecido.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Encapsulamento {
-    class FilhoReconhecido {
+    class FilhoReconhecido : SubCelebridade {
+        public new void MeusAcessos() {
+            Console.WriteLine("** FilhoReconhecido! **");
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
 
+        }
     }
 }
