2c283a46e915676e661e77665c2cbf127a387b07
CursoC# <fipewmc2@gmail.com>
2021-02-23 17:43:16 +0000

    Aula 77 - Encapsulamento -> Seção 7 - Orientação a Objetos

---
+++ Encapsulamento/AmigoProximo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento {
+    class AmigoProximo {
+        public readonly SubCelebridade amiga = new SubCelebridade();
+
+        public void MeusAcessos() {
+            Console.WriteLine(" -- AmigoProximo --");
+            Console.WriteLine();
+        }
+    }
+}
