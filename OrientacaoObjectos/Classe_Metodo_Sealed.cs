562ce6737b90064fe768799bf1bbee162c479e78
CursoC# <fipewmc2@gmail.com>
2021-02-24 11:00:15 +0000

    Aula 81 - Classe e Método Sealed -> Seção 7 - Orientação a Objetos

--- OrientacaoObjectos/Classe_Metodo_Sealed.cs
+++ OrientacaoObjectos/Classe_Metodo_Sealed.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.OrientacaoObjectos {
+
+    sealed class SemFilho {
+        public double ValorDaFortuna() {
+            return 1_487_033.65;
+        }
+    }
+
     class Classe_Metodo_Sealed {
         public static void Executar() {
 
