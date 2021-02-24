e4ac3ff3655ed40daaeb5b36a09ccab2d0965f3d
CursoC# <fipewmc2@gmail.com>
2021-02-24 10:29:46 +0000

    Aula 80 - Interface -> Seção 7 - Orientação a Objetos

--- OrientacaoObjectos/InterfaceOO.cs
+++ OrientacaoObjectos/InterfaceOO.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.OrientacaoObjectos {
+
+    interface OperacaoBinaria {
+        int Operacao(int a, int b);
+    }
+
     class InterfaceOO {
         public static void Executar() {
 
