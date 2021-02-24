0a29804cfd60a8d3dc830bc925f8868a7ce3943c
CursoC# <fipewmc2@gmail.com>
2021-02-24 09:40:15 +0000

    Aula 78 - Polimorfismo -> Seção 7 - Orientação a Objetos

---
+++ OrientacaoObjectos/Polimorfismo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCsharp.OrientacaoObjectos {
+    class Polimorfismo {
+        public static void Executar() {
+
+        }
+    }
+}
