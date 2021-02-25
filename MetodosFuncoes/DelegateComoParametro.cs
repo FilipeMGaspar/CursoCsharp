c5ee34466b7cc3c81abc1dc583503a0b75da32eb
CursoC# <fipewmc2@gmail.com>
2021-02-25 16:06:49 +0000

    Aula 86 - Passando Delegate como Parâmetros -> Seção 8: Métodos & Funções

--- MetodosFuncoes/DelegateComoParametro.cs
+++ MetodosFuncoes/DelegateComoParametro.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace CursoCsharp.MetodosFuncoes {
     class DelegateComoParametro {
+
+        public delegate int Operacao(int x, int y);
+        
         public static void Executar() {
 
         }
