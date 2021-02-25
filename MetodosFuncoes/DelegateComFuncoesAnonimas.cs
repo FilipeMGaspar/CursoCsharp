4f4e69be946313d154b839f21aa8d5562e0ec7bd
CursoC# <fipewmc2@gmail.com>
2021-02-25 15:33:35 +0000

    Aula 85 - Delegate com Funções Anônimas -> Seção 8: Métodos & Funções

--- MetodosFuncoes/DelegateComFuncoesAnonimas.cs
+++ MetodosFuncoes/DelegateComFuncoesAnonimas.cs
@@ -6,7 +6,9 @@ namespace CursoCsharp.MetodosFuncoes {
     class DelegateComFuncoesAnonimas {
         delegate string StringOperacao(string s);
         public static void Executar() {
-
+            StringOperacao inverter = delegate(string s) {
+                char[] charArray
+            }
         }
     }
 }
