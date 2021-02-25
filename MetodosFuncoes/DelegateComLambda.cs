8821e44aefdd5a448f97e9f15a02303f4fe69768
CursoC# <fipewmc2@gmail.com>
2021-02-25 14:57:58 +0000

    Aula 83 - Delegate com Lambda -> Seção 8: Métodos & Funções

--- MetodosFuncoes/DelegateComLambda.cs
+++ MetodosFuncoes/DelegateComLambda.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.MetodosFuncoes {
+
+    delegate double Operacao(double x, double y);
+
     class DelegateComLambda {
         public static void Executar() {
+            Operacao sum = (x, y) => x + y;
 
         }
     }
