fac1dc10dd3aba04cc9088403efea8469bdc73b3
CursoC# <fipewmc2@gmail.com>
2021-02-25 13:47:02 +0000

    Aula 82 - Exemplo Lambda -> Seção 8: Métodos & Funções

--- MetodosFuncoes/ExemploLambda.cs
+++ MetodosFuncoes/ExemploLambda.cs
@@ -21,6 +21,9 @@ namespace CursoCsharp.MetodosFuncoes {
             Func<int, string> conversorHex = numero => numero.ToString("X");
             Console.WriteLine(conversorHex(1234));
 
+            Func<int, int, int, string> formatarData = (dia, mes, ano) =>
+             String.Format("{0:D2}/{1:D2}/{2:D4}", dia, mes, ano);
+
         }
     }
 }
