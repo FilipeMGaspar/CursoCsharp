292399dbd0cade3755dd5770bbd73e8b71d752aa
CursoC# <fipewmc2@gmail.com>
2021-02-25 16:32:05 +0000

    Aula 88 - Métodos de Extensão -> Seção 8: Métodos & Funções

--- MetodosFuncoes/MetodosDeExtensao.cs
+++ MetodosFuncoes/MetodosDeExtensao.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.MetodosFuncoes {
+
+    public static class ExtensoesInteiro {
+        public static int Somar(this int num, int outroNumero) {
+            return num + outroNumero;      
+        }
+
+        public static int Subtracao(this int num, int outroNumero) {
+            return num - outroNumero;
+        }
+    }
+
     class MetodosDeExtensao {
         public static void Executar() {
 
