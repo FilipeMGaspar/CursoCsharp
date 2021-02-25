b1a0738fb749bcdf2b66c7a2e6dfd64327cbd006
CursoC# <fipewmc2@gmail.com>
2021-02-25 15:09:19 +0000

    Aula 84 - Usando Delegates -> Seção 8: Métodos & Funções

--- MetodosFuncoes/UsandoDelegates.cs
+++ MetodosFuncoes/UsandoDelegates.cs
@@ -6,6 +6,7 @@ namespace CursoCsharp.MetodosFuncoes {
     class UsandoDelegates {
 
         delegate double Soma(double a, double b);
+        delegate void ImprimirSoma(double a, double b);
 
         public static void Executar() {
             
