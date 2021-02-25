07157da924466fcf3bbf8807429a812c182c4a5f
CursoC# <fipewmc2@gmail.com>
2021-02-25 16:36:29 +0000

    Aula 88 - Métodos de Extensão -> Seção 8: Métodos & Funções

--- MetodosFuncoes/MetodosDeExtensao.cs
+++ MetodosFuncoes/MetodosDeExtensao.cs
@@ -21,7 +21,8 @@ namespace CursoCsharp.MetodosFuncoes {
             Console.WriteLine(numero.Somar(3));
             Console.WriteLine(numero.Subtracao(10));
 
-            
+            Console.WriteLine(2.Somar(3));
+            Console.WriteLine(5.Subtracao(2));
         }
     }
 }
