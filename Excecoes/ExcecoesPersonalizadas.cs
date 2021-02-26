bd8b417c3a0cd7b20beb410f8f7287b3bca801c4
CursoC# <fipewmc2@gmail.com>
2021-02-26 09:58:30 +0000

    Aula 89 - Criando Exceções Personalizadas -> Seção 9: Exceções

--- Excecoes/ExcecoesPersonalizadas.cs
+++ Excecoes/ExcecoesPersonalizadas.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.Excecoes {
+
+    public class NegativoException : Exception {
+        public NegativoException() { }
+
+        public NegativoException(string message) : base(message) { }
+
+
+    }
+
     class ExcecoesPersonalizadas {
         public static void Executar() {
 
