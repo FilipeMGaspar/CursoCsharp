114875273612f3962429fce4977edd768b0e8786
CursoC# <fipewmc2@gmail.com>
2021-02-26 09:32:28 +0000

    Aula 88 - Exceções e Tratamento de Erro -> Seção 9: Exceções

--- Excecoes/ExcecoesTratamentodErro.cs
+++ Excecoes/ExcecoesTratamentodErro.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.Excecoes {
+
+    public class Conta {
+        private double Saldo;
+
+
+    }
+
     class ExcecoesTratamentodErro {
         public static void Executar() {
 
