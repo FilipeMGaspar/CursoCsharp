21ca25f52fe2c586a34bb6a24dae055ec442404a
CursoC# <fipewmc2@gmail.com>
2021-02-26 12:38:18 +0000

    Aula 91 - Arquivo: Lendo Dados -> Seção 10: Explorando API

--- ExplorandoAPI/LendoDadosDeArquivo.cs
+++ ExplorandoAPI/LendoDadosDeArquivo.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace CursoCsharp.ExplorandoAPI {
+
+    /*public static class Extensaostring {
+        public static string ParseHome(this string path) {
+            string home = (Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX)
+                ? Environment.GetEnvironmentVariable("HOME")
+                : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            return path.Replace("~", home);
+        }
+    }
+    */
     class LendoDadosDeArquivo {
         public static void Executar() {
+            var path = @"~/lendo_arquivos.txt".ParseHome();
+
 
         }
     }
