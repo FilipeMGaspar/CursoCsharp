b154892632ce899de82a3e91513a23fb6e52729b
CursoC# <fipewmc2@gmail.com>
2021-02-26 11:11:47 +0000

    Aula 90 - Arquivo: Escrevendo Dados -> Seção 10: Explorando API

--- ExplorandoAPI/ArquivoEscrevendoDados.cs
+++ ExplorandoAPI/ArquivoEscrevendoDados.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace CursoCsharp.ExplorandoAPI {
     class ArquivoEscrevendoDados {
         public static void Executar() {
-
+            var s = "\test\testetes\a.txt";
+            Console.WriteLine(s);
+            var path = "@~/primeiro_arquivo.txt";
         }
     }
 }
