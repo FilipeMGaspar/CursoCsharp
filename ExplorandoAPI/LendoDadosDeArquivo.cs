0ae8d3c558f7a071402ec06730c6af10284fbbf8
CursoC# <fipewmc2@gmail.com>
2021-02-26 12:41:46 +0000

    Aula 91 - Arquivo: Lendo Dados -> Seção 10: Explorando API

--- ExplorandoAPI/LendoDadosDeArquivo.cs
+++ ExplorandoAPI/LendoDadosDeArquivo.cs
@@ -16,7 +16,11 @@ namespace CursoCsharp.ExplorandoAPI {
         public static void Executar() {
             var path = @"~/lendo_arquivos.txt".ParseHome();
 
-
+            using (StreamWriter sw = File.AppendText(path)) {
+                sw.WriteLine("Produto;Preco;Qtd");
+                sw.WriteLine("Caneta Bic Preta;3.59;89");
+                sw.WriteLine("Borracha Branca;2.89;27");
+            }
         }
     }
 }
