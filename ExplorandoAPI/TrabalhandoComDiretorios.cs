e207cbfca8a40adc6de5eede63a844d7af01067b
CursoC# <fipewmc2@gmail.com>
2021-02-27 11:24:00 +0000

    Aula 93 - Trabalhando com Diretórios -> Seção 10: Explorando API

--- ExplorandoAPI/TrabalhandoComDiretorios.cs
+++ ExplorandoAPI/TrabalhandoComDiretorios.cs
@@ -6,6 +6,15 @@ namespace CursoCsharp.ExplorandoAPI {
         public static void Executar() {
             var novoDir = @"~/PastaCSharp".ParseHome();
             var novoDirDestino = @"~/PastaCSharpDestino".ParseHome();
+            var dirProjectos = @"~/source/repos".ParseHome();
+
+            if (Directory.Exists(novoDir)) {
+                Directory.Delete(novoDir, true);
+            }
+
+            if (Directory.Exists(novoDirDestino)) {
+                Directory.Delete(novoDirDestino, true);
+            }
         }
     }
 }
