d7b9afbb8ba2777e988024d10d9ed9071f3f37c4
CursoC# <fipewmc2@gmail.com>
2021-02-27 11:57:59 +0000

    Aula 94 - Usando DirectoryInfo-> Seção 10: Explorando API

--- ExplorandoAPI/UsandoDirectoryInfo.cs
+++ ExplorandoAPI/UsandoDirectoryInfo.cs
@@ -5,6 +5,21 @@ namespace CursoCsharp.ExplorandoAPI {
     class UsandoDirectoryInfo {
         public static void Executar() {
             var DirProjecto = @"~/source/repos".ParseHome();
+
+            var dirInfo = new DirectoryInfo(DirProjecto);
+
+            if (!dirInfo.Exists) {
+                dirInfo.Create();
+            }
+
+            Console.WriteLine("...: Arquivos :.........");
+            var arquivos = dirInfo.GetFiles();
+
+            foreach (var arquivo in arquivos) {
+                Console.WriteLine(arquivo);
+            }
+
+
         }
     }
 }
