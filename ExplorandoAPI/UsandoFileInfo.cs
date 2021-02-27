2721995671ca2edb9ece35520b0ee9d248f2db87
CursoC# <fipewmc2@gmail.com>
2021-02-27 10:27:33 +0000

    Aula 92 - Usando FileInfo -> Seção 10: Explorando API

--- ExplorandoAPI/UsandoFileInfo.cs
+++ ExplorandoAPI/UsandoFileInfo.cs
@@ -5,7 +5,8 @@ namespace CursoCsharp.ExplorandoAPI {
     class UsandoFileInfo {
         public static void Executar() {
             var caminhoOrigem = @"~/arq_origem.txt".ParseHome();
-            
+            var caminhoDestino = @"~/arq_destino.txt".ParseHome();
+
         }
     }
 }
