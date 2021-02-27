bb237c3718b243c350d8ecead1a5ffdf1915c13a
CursoC# <fipewmc2@gmail.com>
2021-02-27 12:11:15 +0000

    Aula 95 - Usando Path-> Seção 10: Explorando API

--- ExplorandoAPI/UsandoPath.cs
+++ ExplorandoAPI/UsandoPath.cs
@@ -5,6 +5,9 @@ using System.Text;
 namespace CursoCsharp.ExplorandoAPI {
     class UsandoPath {
         public static void Executar() {
+            var arquivo = @"~/exemplo_path_arquivo.txt".ParseHome();
+            var pasta = @"~/exemplo_path_pasta".ParseHome();
+
 
         }
     }
