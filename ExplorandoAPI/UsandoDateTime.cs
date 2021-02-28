44b3f8c0673bc77b6077f5334000bc89f4453219
CursoC# <fipewmc2@gmail.com>
2021-02-28 10:50:31 +0000

    Aula 96 - Usando DateTime -> Seção 10: Explorando API

--- ExplorandoAPI/UsandoDateTime.cs
+++ ExplorandoAPI/UsandoDateTime.cs
@@ -6,9 +6,14 @@ namespace CursoCsharp.ExplorandoAPI {
     class UsandoDateTime {
         public static void Executar() {
             var dateTime = new DateTime(year: 2030, month: 2, day: 6);
+            
             Console.WriteLine(dateTime.Day);
             Console.WriteLine(dateTime.Month);
             Console.WriteLine(dateTime.Year);
+
+            //Sem hora
+            var hoje = DateTime.Today;
+            Console.WriteLine(hoje);
         }
     }
 }
