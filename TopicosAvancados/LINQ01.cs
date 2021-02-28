fcc8dfba8c6cfd474c756b9aaf82107066bfec4d
CursoC# <fipewmc2@gmail.com>
2021-02-28 11:43:51 +0000

    Aula 98 - LINQ #01 -> Seção 11: Tópicos Avançados

--- TopicosAvancados/LINQ01.cs
+++ TopicosAvancados/LINQ01.cs
@@ -16,7 +16,7 @@ namespace CursoCsharp.TopicosAvancados {
                 new Aluno() { Nome ="Pedro", Idade = 24, Nota = 8.0},
                 new Aluno() { Nome ="André", Idade = 21, Nota = 4.3},
                 new Aluno() { Nome ="Ana", Idade = 25, Nota = 9.5},
-                new Aluno() { Nome ="Jorge", Idade = 28, Nota = 8.5},
+                new Aluno() { Nome ="Jorge", Idade = 20, Nota = 8.5},
                 new Aluno() { Nome ="Ana", Idade = 21, Nota = 7.7},
                 new Aluno() { Nome ="Julia", Idade = 22, Nota = 7.5},
                 new Aluno() { Nome ="Marcio", Idade = 18, Nota = 6.8},
