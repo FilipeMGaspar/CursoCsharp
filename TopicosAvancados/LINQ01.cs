ad16e9bb542d6f4ae2f910f9b5a57f649d7c4049
CursoC# <fipewmc2@gmail.com>
2021-02-28 11:33:03 +0000

    Aula 98 - LINQ #01 -> Seção 11: Tópicos Avançados

--- TopicosAvancados/LINQ01.cs
+++ TopicosAvancados/LINQ01.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.TopicosAvancados {
+
+    public class Aluno {
+        public string Nome;
+        public int Idade;
+        public double Nota;
+    } 
+
     class LINQ01 {
         public static void Executar() {
 
