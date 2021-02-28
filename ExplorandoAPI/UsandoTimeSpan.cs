1b7b1fcad212166efe81d87d18018c49b60e43e0
CursoC# <fipewmc2@gmail.com>
2021-02-28 11:17:55 +0000

    Aula 97 - Usando TimeSpan -> Seção 10: Explorando API

--- ExplorandoAPI/UsandoTimeSpan.cs
+++ ExplorandoAPI/UsandoTimeSpan.cs
@@ -8,11 +8,14 @@ namespace CursoCsharp.ExplorandoAPI {
             var intervalo = new TimeSpan(days: 10, hours: 20, minutes: 30, seconds: 40);
             Console.WriteLine(intervalo);
 
-            Console.WriteLine("\nMinutos: " +intervalo.Minutes);
+            Console.WriteLine("\nMinutos: " + intervalo.Minutes);
             Console.WriteLine("Intervalo em Minutos: " + intervalo.TotalMinutes);
 
             var largada = DateTime.Now;
             var chegada = DateTime.Now.AddMinutes(15);
+
+            var tempo = chegada - largada;
+            Console.WriteLine(tempo);
         }
     }
 }
