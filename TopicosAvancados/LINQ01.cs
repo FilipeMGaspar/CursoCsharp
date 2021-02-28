9d550391d9dd053a41f5450a354ee2c7700ac04d
CursoC# <fipewmc2@gmail.com>
2021-02-28 11:41:08 +0000

    Aula 98 - LINQ #01 -> Seção 11: Tópicos Avançados

--- TopicosAvancados/LINQ01.cs
+++ TopicosAvancados/LINQ01.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace CursoCsharp.TopicosAvancados {
 
@@ -12,7 +12,7 @@ namespace CursoCsharp.TopicosAvancados {
 
     class LINQ01 {
         public static void Executar() {
-            var alunos = new List<Aluno> { 
+            var alunos = new List<Aluno> {
                 new Aluno() { Nome ="Pedro", Idade = 24, Nota = 8.0},
                 new Aluno() { Nome ="André", Idade = 21, Nota = 4.3},
                 new Aluno() { Nome ="Ana", Idade = 25, Nota = 9.5},
@@ -20,8 +20,14 @@ namespace CursoCsharp.TopicosAvancados {
                 new Aluno() { Nome ="Ana", Idade = 21, Nota = 7.7},
                 new Aluno() { Nome ="Julia", Idade = 22, Nota = 7.5},
                 new Aluno() { Nome ="Marcio", Idade = 18, Nota = 6.8},
-            }
+            };
 
+            Console.WriteLine("=== Aprovados ==========");
+            var aprovados = alunos.Where(a => true);
+
+            foreach (var aluno in aprovados) {
+                Console.WriteLine(aluno.Nome);
+            }
         }
     }
 }
