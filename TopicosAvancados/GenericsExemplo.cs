473c319fb13a637b4319039a28ff6620fa14291c
CursoC# <fipewmc2@gmail.com>
2021-03-01 13:24:09 +0000

    Aula 102 - Generics -> Seção 11: Tópicos Avançados

--- TopicosAvancados/GenericsExemplo.cs
+++ TopicosAvancados/GenericsExemplo.cs
@@ -10,7 +10,15 @@ namespace CursoCsharp.TopicosAvancados {
         public T Coisa { get; set; }
 
         public Caixa(T coisa) {
+            Coisa = coisa;
+        }
 
+        public T metodoGenerico(T valor) {
+            return new Random().Next(0, 2) ? Coisa : valor;
+        }
+
+        public T GetValor() {
+            return valorPrivado;
         }
     }
 
