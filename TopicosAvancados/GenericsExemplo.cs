b36480f46d0de91ef36aecfae95c6229908a5bb8
CursoC# <fipewmc2@gmail.com>
2021-03-01 13:22:03 +0000

    Aula 102 - Generics -> Seção 11: Tópicos Avançados

--- TopicosAvancados/GenericsExemplo.cs
+++ TopicosAvancados/GenericsExemplo.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCsharp.TopicosAvancados {
+
+    public class Caixa<T> {
+        T valorPrivado;
+        
+        public T Coisa { get; set; }
+
+        public Caixa(T coisa) {
+
+        }
+    }
+
     class GenericsExemplo {
         public static void Executar() {
 
